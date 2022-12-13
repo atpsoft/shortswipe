c0dd344d7f34af6961382413f5d06fc1b4b03b31
pquimo <pquimo@users.noreply.github.com>
2022-12-13 15:56:02 -0800

    Initial Commit

---
+++ ShortSwipe/ShortSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortSwipeApp.swift
+//  ShortSwipe
+//
+//  Created by Makani Mason on 12/13/22.
+//
+
+import SwiftUI
+
+@main
+struct ShortSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
