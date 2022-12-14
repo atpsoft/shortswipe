257665611cadb19fcf0f89171169d056b220d13a
pquimo <pquimo@users.noreply.github.com>
2022-12-13 16:04:37 -0800

    standard SwiftUI swipe

--- ShortSwipe/ShortSwipeApp.swift
+++ ShortSwipe/ShortSwipeApp.swift
@@ -1,17 +1,27 @@
-//
-//  ShortSwipeApp.swift
-//  ShortSwipe
-//
-//  Created by Makani Mason on 12/13/22.
-//
-
 import SwiftUI
+
+struct ContentView: View {
+  @State var items = [1, 2, 3]
+  var body: some View {
+    List {
+      ForEach(0..<10) { index in
+        Text("Item \(index)")
+          .swipeActions(edge: .leading) {
+            Button(
+              action: { print("swiped \(index)") },
+              label: { Image(systemName: "star") }
+            )
+          }
+      }
+    }
+  }
+}
 
 @main
 struct ShortSwipeApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
